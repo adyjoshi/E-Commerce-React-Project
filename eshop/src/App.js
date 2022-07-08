870674fa4b597dd0a02553351b95e83356179cdc
Aditi Joshi <38169015+adyjoshi@users.noreply.github.com>
2022-07-07 22:45:13 -0230

    Edit eshop header

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,9 +1,27 @@
-
 import './App.css';
 
-function App() {
-  return (
+function App(){
+  return(
     <div className="App">
+      <div className="header__logo"></div>
+      <div className="header__search"></div>
+      <div className="header__nav">
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign In</span>
+        </div>
+        <div className="nav__item">
+        <span className="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign In</span>
+        </div>
+        <div className="nav__item">
+        <span className="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign In</span>
+        </div>
+      </div>
+
+
+
     </div>
   );
 }
